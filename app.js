ab3e02ba4c6179bf82d479959e84a41cd0a3275b
jeffsford <jeff@jeffford.com>
2017-03-13 18:20:37 -0700

    Updated firstAndPike object

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 'use strict';
 
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
 var firstAndPike = {
   minCust: 23,
   maxCust: 65,
   avgCookies: 6.3,
+  hourlySales: [],
   calcCookieSales: function(){
     return Math.floor(Math.random() * ((this.maxCust - this.minCust + 1) + this.minCust));
   },
   cookiesPerHour: function() {
-    var totalSales = [];
     for (i = 0; i < hours.length; i++) {
-      totalSales.push((this.avgCookies * this.calcCookieSales()).toFixed(0));
+      this.hourlySales.push((this.avgCookies * this.calcCookieSales()).toFixed(0));
     }
-    console.log(totalSales);
   },
+  totalSales: function() {
+    var dailySales = 0;
+    for (i = 0; i < this.hourlySales.length; i++) {
+      dailySales += int(this.hourlySales[i]);
+      console.log('hourly sale at ' + i + ': ' + this.hourlySales[i] );
+      console.log('daily sales at ' + i + ': ' + dailySales);
+    }
+    return dailySales;
+  }
 };
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
