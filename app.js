442451972e649cfeff7ed33a5cd83f0c11459190
jeffsford <jeff@jeffford.com>
2017-03-13 18:34:16 -0700

    Additional objects added

--- app.js
+++ app.js
@@ -25,3 +25,99 @@ var firstAndPike = {
     return dailySales;
   }
 };
+
+var seaTac = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookies: 1.2,
+  hourlySales: [],
+  calcCookieSales: function(){
+    return Math.floor(Math.random() * ((this.maxCust - this.minCust + 1) + this.minCust));
+  },
+  cookiesPerHour: function() {
+    for (i = 0; i < hours.length; i++) {
+      this.hourlySales.push((this.avgCookies * this.calcCookieSales()).toFixed(0));
+    }
+  },
+  totalSales: function() {
+    var dailySales = 0;
+    for (i = 0; i < this.hourlySales.length; i++) {
+      dailySales += int(this.hourlySales[i]);
+      console.log('hourly sale at ' + i + ': ' + this.hourlySales[i] );
+      console.log('daily sales at ' + i + ': ' + dailySales);
+    }
+    return dailySales;
+  }
+};
+
+var seaCenter = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookies: 3.7,
+  hourlySales: [],
+  calcCookieSales: function(){
+    return Math.floor(Math.random() * ((this.maxCust - this.minCust + 1) + this.minCust));
+  },
+  cookiesPerHour: function() {
+    for (i = 0; i < hours.length; i++) {
+      this.hourlySales.push((this.avgCookies * this.calcCookieSales()).toFixed(0));
+    }
+  },
+  totalSales: function() {
+    var dailySales = 0;
+    for (i = 0; i < this.hourlySales.length; i++) {
+      dailySales += int(this.hourlySales[i]);
+      console.log('hourly sale at ' + i + ': ' + this.hourlySales[i] );
+      console.log('daily sales at ' + i + ': ' + dailySales);
+    }
+    return dailySales;
+  }
+};
+
+var capHill = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookies: 2.3,
+  hourlySales: [],
+  calcCookieSales: function(){
+    return Math.floor(Math.random() * ((this.maxCust - this.minCust + 1) + this.minCust));
+  },
+  cookiesPerHour: function() {
+    for (i = 0; i < hours.length; i++) {
+      this.hourlySales.push((this.avgCookies * this.calcCookieSales()).toFixed(0));
+    }
+  },
+  totalSales: function() {
+    var dailySales = 0;
+    for (i = 0; i < this.hourlySales.length; i++) {
+      dailySales += int(this.hourlySales[i]);
+      console.log('hourly sale at ' + i + ': ' + this.hourlySales[i] );
+      console.log('daily sales at ' + i + ': ' + dailySales);
+    }
+    return dailySales;
+  }
+};
+
+var alki = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookies: 4.6,
+  hourlySales: [],
+  calcCookieSales: function(){
+    return Math.floor(Math.random() * ((this.maxCust - this.minCust + 1) + this.minCust));
+  },
+  cookiesPerHour: function() {
+    for (i = 0; i < hours.length; i++) {
+      this.hourlySales.push((this.avgCookies * this.calcCookieSales()).toFixed(0));
+    }
+  },
+  totalSales: function() {
+    var dailySales = 0;
+    for (i = 0; i < this.hourlySales.length; i++) {
+      dailySales += int(this.hourlySales[i]);
+      console.log('hourly sale at ' + i + ': ' + this.hourlySales[i] );
+      console.log('daily sales at ' + i + ': ' + dailySales);
+    }
+    return dailySales;
+  }
+};
